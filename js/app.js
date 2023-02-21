c040efafb84682f300f6e1df73fca0374fc38cd5
borno360 <115914996+borno360@users.noreply.github.com>
2023-02-21 13:17:20 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,16 @@
+
+
+$(document).ready(function(){
+   $('.banner-class').slick({
+     settingName: setting-value
+   });
+   $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+    });
+          
+ });
+ 
